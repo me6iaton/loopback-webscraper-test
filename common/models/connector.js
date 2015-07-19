53a0b217aa50811bf20d98ddde72e8fe3e139382
me6iaton <me6iaton@gmail.com>
2015-07-19 23:23:23 +0300

    add fetch method for Connector model

--- common/models/connector.js
+++ common/models/connector.js
@@ -1,3 +1,39 @@
+var osmosis = require('osmosis');
+
 module.exports = function(Connector) {
 
+  Connector.fetch = function (connectorId, cb){
+    Connector.findById( connectorId, function (err, instance) {
+      var response = [];
+      var scraper = osmosis.get(instance.url);
+
+      for (var item of instance.chainScraping){
+        var action = Object.keys(item)[0];
+        scraper = scraper[action](item[action]);
+      }
+
+      scraper
+        .log(console.log)
+        .error(function(msg){
+          throw msg
+        })
+        .data(function(data) {
+          //console.log(data);
+          response.push(data);
+        })
+        .done(function(){
+          cb(null, response)
+        });
+
+    });
+  };
+
+  Connector.remoteMethod (
+  'fetch',
+  {
+    http: {path: '/fetch', verb: 'get'},
+    accepts: {arg: 'id', type: 'number', required: true, http: { source: 'query' } },
+    returns: {arg: 'response', type: 'array'}
+  });
 };
+
