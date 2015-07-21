83e8264158fc436da3d9387bc7b4473abc9ba266
me6iaton <me6iaton@gmail.com>
2015-07-21 18:03:17 +0300

    add scheduler hook - stop or start fetch job after active property update

--- server/boot/scheduler.js
+++ server/boot/scheduler.js
@@ -1,8 +1,11 @@
 var CronJob = require('cron').CronJob;
 
 module.exports = function(app) {
-  var jobMap = new WeakMap();
-  app.models.Connector.find({}, function(err, connectors){
+
+  var jobs = [];
+
+  // get all connectors, create fetching cron jobs, save then in array and start if active.
+  app.models.Connector.find(function(err, connectors){
     if(err) console.error(err);
     for (var connector of connectors){
       if (connector.schedule){
@@ -13,13 +16,32 @@ module.exports = function(app) {
               console.log('shedule tick!');
               console.log(results);
             });
-
           },
           context: connector,
-          start: true
+          start: false
         });
-        jobMap.set(connector,job);
+        jobs[connector.id] = job;
+        if (connector.active === true){
+          job.start();
+          console.log('fetching job with id '+connector.id+' start');
+        }
       }
     }
-  })
+  });
+
+  // scheduler after save hook - stop or start fetching jobs after active property change
+  app.models.Connector.observe('after save', function(ctx, next) {
+    if(ctx.instance.active === true){
+      jobs[ctx.instance.id].start();
+      console.log('fetching job with id '+ctx.instance.id+' start');
+    } else if (ctx.instance.active === false){
+      jobs[ctx.instance.id].stop();
+      console.log('fetching job with id '+ctx.instance.id+' stop');
+    }
+    next();
+  });
+
+
+
 };
+
