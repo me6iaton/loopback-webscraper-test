93a185041aa292be267b336cbacf123960ef1fdf
me6iaton <me6iaton@gmail.com>
2015-07-22 15:11:26 +0300

    change skipAlreadyScrapedData - replace Connector.scrapCheckpoit check to Record.created propery check

--- common/models/connector.js
+++ common/models/connector.js
@@ -5,52 +5,21 @@ module.exports = function(Connector) {
   // fetch data from url, scraper and follow to link recursive
   // see https://github.com/rc0x03/node-osmosis
   Connector.prototype.fetch = function (cb){
-
-    // filter scraped data function
-    var skipAlreadyScrapedData = (function(){
-      var firstSetFlag = false,
-      firstRecordFlag = true,
-      skipDataFlag = false,
-      scrapCheckpoint = null;
-      return function(connector, scraper, action){
-        if(!scrapCheckpoint) scrapCheckpoint = connector.scrapCheckpoint;
-        // waiting first set command in chain, and filter func only this level
-        if(action == 'set' && !firstSetFlag){
-          firstSetFlag = true;
-          scraper = scraper.then(function(context, data, next) {
-            // choice first scraped for сheckpoint
-            if(JSON.stringify(data) === JSON.stringify(scrapCheckpoint)){
-              skipDataFlag = true;
-            }
-            // update scrapCheckpoint prop in Connector instance
-            if (firstRecordFlag && !skipDataFlag){
-              connector.updateAttribute('scrapCheckpoint', data, function(err){
-                if(err) console.error(err);
-              });
-              firstRecordFlag = false;
-            }
-            if(!skipDataFlag){
-              next(context, data);
-            }else{
-              next(null);
-            }
-          })
-        }
-        return scraper
-      };
-    })();
-
-    var results = [],
-    scraper = osmosis.get(this.url);
-    // build promise chain from chainScraping property in Connector instance
-    // see https://github.com/rc0x03/node-osmosis
-    for (var item of this.chainScraping){
-      var action = Object.keys(item)[0];
-      scraper = scraper[action](item[action]);
-      // do not fetch already stored data
-      scraper = skipAlreadyScrapedData(this, scraper, action)
-    }
-    scraper
+    var self = this;
+    // first get last record date for already stored check
+    Connector.app.models.Record.findOne({order: 'created DESC', where: {connector: self.id}}, function(err, record){
+      if(err) return console.error(err);
+      var results = [],
+      scraper = osmosis.get(self.url);
+      // build promise chain from chainScraping property in Connector instance
+      // see https://github.com/rc0x03/node-osmosis
+      for (var item of self.chainScraping){
+        var action = Object.keys(item)[0];
+        scraper = scraper[action](item[action]);
+        // do not fetch already stored data
+        scraper = skipAlreadyScrapedData(self, record, scraper, action)
+      }
+      scraper
       .error(function(msg){
         console.error(msg)
       })
@@ -67,6 +36,39 @@ module.exports = function(Connector) {
         }
         if (cb) cb(null, results);
       });
+    });
+
+    // filter scraped data
+    var skipAlreadyScrapedData = (function(){
+      var firstSetFlag = false,
+      skipDataFlag = false,
+      created = Date.now();
+      return function(connector, record, scraper, action){
+        // for first time
+        if(!record) record = {url: null};
+
+        // waiting first set command in chain, and filter func only this level
+        if(action == 'set' && !firstSetFlag){
+          firstSetFlag = true;
+          scraper = scraper.then(function(context, data, next) {
+            // set created date for sorting
+            created = created - 1;
+            data.created = new Date(created);
+            // find first data already stored
+            if(data.url === record.url){
+              skipDataFlag = true;
+            }
+            if(!skipDataFlag){
+              next(context, data);
+            }else{
+              next(null);
+            }
+          })
+        }
+        return scraper
+      };
+    })();
+
   };
 
   Connector.remoteMethod (
--- server/boot/hooks.js
+++ server/boot/hooks.js
@@ -4,17 +4,19 @@ module.exports = function(app) {
   app.models.Connector.afterRemote('prototype.fetch', function(ctx, results, next){
     if (!results) return next();
     var records = [];
-    var date =  new Date();
     for (var item of results){
       records.push({
         connector: ctx.instance.id,
-        created: date,
+        created: item.created,
+        url: item.url,
         data: item
       })
     }
-    app.models.Record.create(records, function(err){
-      if (err)
+    app.models.Record.upsert(records, function(err){
+      if (err){
+        console.error(err);
         next(err);
+      }
       next();
     });
   });
