a6944e83710de9f22875b487ea92a564aac50f7f
me6iaton <me6iaton@gmail.com>
2015-07-23 14:38:54 +0300

    refactor Connector model, replace fetch metod to  scrapeAndCreateRecords, add scrapeAndUpdateOrCreateRecords metod, add hooks for this.

--- common/models/connector.js
+++ common/models/connector.js
@@ -1,79 +1,130 @@
 var osmosis = require('osmosis');
 
+/**
+ * build promise chain for scraping from chainScraping property in Connector instance
+ * see https://github.com/rc0x03/node-osmosis
+ * fetch data from url, scraped and follow to links
+ * @param ctx context
+ * @param {Object} ctx.connector Connector instance
+ * @param {Object} [ctx.record] Record instance
+ * @param cb callback
+ */
+var scrape = function(ctx, cb){
+  var connector = ctx.connector;
+  var results = [],
+  handleScrapData = handleScrapDataConstructor(ctx),
+  scraper = osmosis.get(connector.url);
+  // build promise chain from chainScraping property in Connector instance
+  // see https://github.com/rc0x03/node-osmosis
+  for (var item of connector.chainScraping){
+    var action = Object.keys(item)[0];
+    scraper = scraper[action](item[action]);
+    scraper = handleScrapData(scraper, action)
+  }
+  scraper
+  .error(function(msg){
+    console.error(msg)
+  })
+  .data(function(data) {
+    results.push(data);
+  })
+  .done(function(){
+    // sort results by published field, because follow command performed concurrency
+    // set scraper.config('concurrency', 1) for disable this
+    results.sort(function(a,b){
+      return new Date(b.created) - new Date(a.created);
+    });
+    if (cb) cb(null, results);
+  });
+};
+
+/**
+ * Constructor handleScrapData, run before fetch crawl links
+ * created date for sorting after concurrency crawl and prevent crawl already stored links
+ * @param ctx
+ * @param {Object} [ctx.record] first Record instance {order: 'created DESC', where: {connector: connector.id}
+ * @returns {Function}
+ */
+var handleScrapDataConstructor = function(ctx){
+  var firstSetFlag = false,
+  skipDataFlag = false,
+  created = Date.now(),
+  record;
+
+  if(ctx.record)
+    // skip fetching already stored data, filter by last stored record
+    record = ctx.record;
+  else
+    // don't skip fetching for all data if scrape run first time or update stored data
+    record = {url: null};
+
+  /**
+   * @param {Object} scraper
+   * @param {string} action
+   */
+  return function(scraper, action){
+
+    // waiting first "set" command in chain, and run filter function only this level in chain
+    if(action == 'set' && !firstSetFlag){
+      firstSetFlag = true;
+      scraper = scraper.then(function(context, data, next) {
+
+        // set created date for sorting and find last stored record
+        created = created - 1;
+        data.created = new Date(created);
+
+        // not fetch already stored data
+        if(data.url === record.url){
+          skipDataFlag = true
+        }
+        if(skipDataFlag){
+          next(null)
+        }else{
+          next(context, data)
+        }
+
+      })
+    }
+    return scraper
+  };
+};
+
 module.exports = function(Connector) {
 
-  // fetch data from url, scraper and follow to link recursive
-  // see https://github.com/rc0x03/node-osmosis
-  Connector.prototype.fetch = function (cb){
-    var self = this;
-    // first get last record date for already stored check
-    Connector.app.models.Record.findOne({order: 'created DESC', where: {connector: self.id}}, function(err, record){
+  /**
+   * scrapeAndCreateRecords
+   * @param cb
+   */
+  Connector.prototype.scrapeAndCreateRecords = function (cb){
+    var connector = this;
+    // first get last record, not fetch already stored data check
+    Connector.app.models.Record.findOne({order: 'created DESC', where: {connector: connector.id}}, function(err, record){
       if(err) return console.error(err);
-      var results = [],
-      scraper = osmosis.get(self.url);
-      // build promise chain from chainScraping property in Connector instance
-      // see https://github.com/rc0x03/node-osmosis
-      for (var item of self.chainScraping){
-        var action = Object.keys(item)[0];
-        scraper = scraper[action](item[action]);
-        // do not fetch already stored data
-        scraper = skipAlreadyScrapedData(self, record, scraper, action)
-      }
-      scraper
-      .error(function(msg){
-        console.error(msg)
-      })
-      .data(function(data) {
-        results.push(data);
-      })
-      .done(function(){
-        // sort results by published field, because follow command performed concurrency
-        // set scraper.config('concurrency', 1) for disable this
-        results.sort(function(a,b){
-          return new Date(b.created) - new Date(a.created);
-        });
-        if (cb) cb(null, results);
-      });
+      scrape({connector: connector, record: record}, cb);
     });
+  };
 
-    // filter scraped data
-    var skipAlreadyScrapedData = (function(){
-      var firstSetFlag = false,
-      skipDataFlag = false,
-      created = Date.now();
-      return function(connector, record, scraper, action){
-        // for first time
-        if(!record) record = {url: null};
-
-        // waiting first set command in chain, and filter func only this level
-        if(action == 'set' && !firstSetFlag){
-          firstSetFlag = true;
-          scraper = scraper.then(function(context, data, next) {
-            // set created date for sorting
-            created = created - 1;
-            data.created = new Date(created);
-            // find first data already stored
-            if(data.url === record.url){
-              skipDataFlag = true;
-            }
-            if(!skipDataFlag){
-              next(context, data);
-            }else{
-              next(null);
-            }
-          })
-        }
-        return scraper
-      };
-    })();
-
+  /**
+   *  scrapeAndUpdateOrCreateRecords
+   * @param cb callback
+   */
+  Connector.prototype.scrapeAndUpdateOrCreateRecords = function (cb){
+    scrape({connector: this}, cb);
   };
 
   Connector.remoteMethod (
-  'fetch',
+  'scrapeAndCreateRecords',
   {
     isStatic: false,
-    http: {path:'/fetch', verb: 'get'},
+    http: {path:'/scrape', verb: 'post'},
+    returns: {arg: 'results', type: 'array', root: true}
+  });
+
+  Connector.remoteMethod (
+  'scrapeAndUpdateOrCreateRecords',
+  {
+    isStatic: false,
+    http: {path:'/scrape', verb: 'put'},
     returns: {arg: 'results', type: 'array', root: true}
   });
 };
--- server/boot/hooks.js
+++ server/boot/hooks.js
@@ -1,24 +1,65 @@
+var _ = require('lodash');
+
+/**
+ * handle Record before create
+ * @param {Object} ctx
+ * @param {Object} ctx.instance
+ * @param {Object} record
+ * @returns {{connector: *, url: *, created: (*|Date), data: *}}
+ */
+var handleRecordBeforeCreate = function(ctx, record){
+  return {
+    connector: ctx.instance.id,
+    url: record.url,
+    created: record.created,
+    data: record
+  }
+};
+
 module.exports = function(app) {
 
-  // store data after fetch and scraper url.
-  app.models.Connector.afterRemote('prototype.fetch', function(ctx, results, next){
+  // Connector hook create Records after fetch and scrape url.
+  app.models.Connector.afterRemote('prototype.scrapeAndCreateRecords', function(ctx, results, next){
     if (!results) return next();
-    var records = [];
-    for (var item of results){
-      records.push({
-        connector: ctx.instance.id,
-        url: item.url,
-        created: item.created,
-        data: item
-      })
-    }
-    app.models.Record.upsert(records, function(err){
+    results.forEach(function(record, index, results){
+      results[index] = handleRecordBeforeCreate(ctx, record)
+    });
+    app.models.Record.create(results, function(err){
       if (err){
         console.error(err);
         next(err);
       }
       next();
     });
+  });
+
+  // Connector hook update or create Records after fetch and scrape url.
+  app.models.Connector.afterRemote('prototype.scrapeAndUpdateOrCreateRecords', function(ctx, results, next){
+    var records,
+    jobs = [];
+    app.models.Record.find({where: {connector: ctx.instance.id}})
+    .then(function(instances){
+      records = instances;
+    })
+    .then(function(){
+      for (var result of results) {
+        var record = _.find(records, 'url', result.url);
+        if(record){
+          delete result.created;
+          jobs.push(record.updateAttributes({data: _.assign(record.__data.data, result)}));
+        }else{
+          jobs.push(app.models.Record.create(handleRecordBeforeCreate(ctx, result)))
+        }
+      }
+      return Promise.all(jobs);
+    })
+    .then(function(){
+      next()
+    })
+    .catch(function(err){
+      next(err);
+      console.error(err)
+    })
   });
 
 };
